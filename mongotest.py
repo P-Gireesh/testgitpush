4d850d7c355f2af768033e2bf06b9ca46906debc
P-Gireesh <hari.giree2014@gmail.com>
2022-09-10 17:52:05 +0530

    this is my first code push in git

--- mongotest.py
+++ mongotest.py
@@ -1,6 +1,8 @@
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://<gireesh22>:mitravinda22@cluster0.su6prrl.mongodb.net/?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient("mongodb+srv://gireesh22:mitravinda22@cluster0.su6prrl.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
+
 print(db)
 
 d = {
@@ -11,3 +13,4 @@ d = {
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
+
